c4a8f31d4260b541c55457e9582d56bd12c6c966
Dimitar Kokotanekov <acid901@mail.bg>
2017-05-07 19:29:19 +0300

    initial Adventure commit

---
+++ Adventure/Adventure/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Adventure.MvcApplication" Language="C#" %>
---
+++ Adventure/Adventure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Adventure.Startup))]
+namespace Adventure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
